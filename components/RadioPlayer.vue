8baafb69648493bced5fad90aaaea443643bc37b
rhzd <i.ruhaizad@gmail.com>
2021-08-26 23:04:53 +0800

    test

--- components/RadioPlayer.vue
+++ components/RadioPlayer.vue
@@ -41,8 +41,10 @@
         class="square"
         alt="Current song"
         :src="
-          currentMetadata && currentMetadata.coverUrl
+          currentMetadata
             ? currentMetadata.coverUrl
+              ? currentMetadata.coverUrl
+              : station.logo
             : station.logo
         "
       />
@@ -179,30 +181,90 @@
         @click="shareActive = !shareActive"
       />
     </div>
-    <Audio :stream="station.stream" />
-    <!-- <audio
-      ref="audio"
-      @canplay="updatePaused"
-      @playing="updatePaused"
-      @pause="updatePaused"
-    ></audio> -->
+    <audio class="op-player__media" id="audio" controls>
+      <source :src="`${station.stream}&listenerid=${listenerId}`" />
+    </audio>
   </div>
 </template>
 
 <script>
+import OpenPlayerJS from "openplayerjs";
+
 export default {
-  props: ["station", "host", "gotPlayoutHistory"],
+  head() {
+    return {
+      script: [
+        {
+          src: "https://synchrobox.adswizz.com/register2.php",
+          defer: true,
+        },
+        {
+          src: "https://cdn.adswizz.com/adswizz/js/SynchroClient2.js",
+          defer: true,
+        },
+      ],
+    };
+  },
+  props: ["station", "gotPlayoutHistory", "host"],
   data() {
     return {
       loading: true,
       paused: true,
       shareActive: false,
       volume: 30,
-      audio: null,
       currentMetadata: null,
       isHlsSupported: false,
-      componentKey: 0,
+      $player: null,
+      stream: null,
+      listenerId: null,
     };
+  },
+  mounted() {
+    this.listenerId = com_adswizz_synchro_getListenerId();
+    this.$player = new OpenPlayerJS("audio", {
+      hls: {
+        startLevel: -1,
+      },
+      showLoaderOnInit: false,
+      pauseOthers: true,
+    });
+    console.log(this.$player);
+    this.$player.init();
+    this.$player
+      .getElement()
+      .addEventListener("hlsFragParsingMetadata", (event) => {
+        let dict = {};
+        let tmp = event.detail.data.frag.title.split("=");
+        if (tmp.length > 0) {
+          if (tmp[0] == "title") {
+            dict["title"] = tmp[1].replace(",url", "").replace(/"/g, "").trim();
+          }
+          if (tmp.length == 3 && tmp[2].toString()[1] === "%") {
+            try {
+              dict["data"] = JSON.parse(
+                decodeURIComponent(tmp[2].substring(1, tmp[2].length - 1))
+              );
+            } catch (err) {
+              console.error("Metadata Parsing Error", err);
+              console.error(decodeURIComponent(tmp[2].replace('"', "")));
+            }
+          }
+        }
+        if (dict.data) {
+          if (this.currentMetadata) {
+            if (dict.data.current_song.track !== this.currentMetadata.track) {
+              this.$root.$refs.LastPlayedSong.fetchPlayoutHistory(
+                dict.data.current_song
+              );
+            }
+          } else {
+            this.$root.$refs.LastPlayedSong.fetchPlayoutHistory(
+              dict.data.current_song
+            );
+          }
+        }
+        this.currentMetadata = dict.data ? dict.data.current_song : null;
+      });
   },
   computed: {
     playing() {
@@ -210,9 +272,6 @@ export default {
     },
   },
   methods: {
-    forceRerender() {
-      this.componentKey += 1;
-    },
     shareToggle() {
       this.shareActive = !this.shareActive;
     },
@@ -224,11 +283,6 @@ export default {
       this.audio = event.target;
       this.paused = event.target.paused;
       this.loading = false;
-      if (Hls.isSupported()) {
-        this.isHlsSupported = true;
-      } else {
-        this.isHlsSupported = false;
-      }
     },
     play() {
       this.audio.play();
@@ -281,6 +335,7 @@ export default {
       alert(`Copied ${this.host} to clipboard!`);
     },
   },
+  watch: {},
 };
 </script>
 
